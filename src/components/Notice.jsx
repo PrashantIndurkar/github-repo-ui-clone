52a7f584715eafc05a72c9a889e6ec7d5621d865
prashant indurkar <prashantri2000@gmail.com>
2022-12-16 16:51:48 +0530

    added notice and changed color

--- src/components/Notice.jsx
+++ src/components/Notice.jsx
@@ -1,6 +1,6 @@
 const Notice = () => {
   return (
-    <div className="py-1 font-inherit text-center w-full bg-[#f8b4b4]">
+    <div className="py-1 font-inherit text-center w-full bg-[#ffdfdf]">
       It's not a original github website its a clone
     </div>
   );
