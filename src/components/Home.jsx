5bd80ed0725af022405e001dfcd333e86d986a32
prashant indurkar <prashantri2000@gmail.com>
2022-12-16 16:49:04 +0530

    added notice

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,11 @@
 import Header from "./Header";
 import Navbar from "./Navbar";
 import RepoSection from "./RepoSection";
+import Notice from "./Notice";
 const Home = () => {
   return (
     <>
+      <Notice />
       <Navbar />
       <Header />
       <RepoSection />
---
+++ src/components/Notice.jsx
@@ -0,0 +1,9 @@
+const Notice = () => {
+  return (
+    <div className="py-1 font-inherit text-center w-full bg-[#f8b4b4]">
+      It's not a original github website its a clone
+    </div>
+  );
+};
+
+export default Notice;
